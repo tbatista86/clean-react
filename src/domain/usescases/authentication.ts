8db57e5cff9420c7bd9b8ab5669f9ec089919a9b
Tiago Batista <tiagobatista86@gmail.com>
2020-07-22 06:28:34 -0300

    chore: add pakage json with version course

---
+++ src/domain/usescases/authentication.ts
@@ -0,0 +1,9 @@
+// import { AccountModel } from '../models/account-models';
+
+// type AutheticationParams = {
+//   email: string;
+//   password: string;
+// };
+// export interface Authentication {
+//   auth(params: AutheticationParams): Promise<AccountModel>;
+// }
