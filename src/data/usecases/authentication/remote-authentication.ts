2482f970fbfb63342f32eaa64483e5d94f6cbb62
Tiago Batista <tiagobatista86@gmail.com>
2020-07-24 06:19:04 -0300

    feat: ensere RemoteAuthentication throws InvalidCredentialsErros with 401

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,5 +1,7 @@
 import { HttpPostClient } from '../../protocols/http/http-post-client'
 import { AutheticationParams } from '../../../domain/usescases/authentication'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { InvalidCredentialError } from '@/domain/errors/invalid-credentials-error'
 
 export class RemoteAuthentication {
   constructor (
@@ -8,9 +10,16 @@ export class RemoteAuthentication {
   ) {}
 
   async auth (params: AutheticationParams): Promise<void> {
-    await this.httpPostClient.post({
+    const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.unathorized:
+        throw new InvalidCredentialError()
+      default:
+        return await Promise.resolve()
+    }
   }
 }
